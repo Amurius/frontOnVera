b3f07883b69690280167b180757e4a0315a2d197
Amurius <124882829+Amurius@users.noreply.github.com>
2025-12-03 14:12:23 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://back-on-vera.vercel.app/api',
+  apiUrl: 'https://sondage-backend.onrender.com/api',
   cryptoKey: '2d407bb670c5765215233b0eca52493645ac109077344afaaadfc5b489e36a38',
 };
